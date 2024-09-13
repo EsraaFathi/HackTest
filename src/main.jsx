32570b7b3c7270a7530c83e87332063606ba92cb
EsraaFathi <138526584+EsraaFathi@users.noreply.github.com>
2024-09-13 21:45:27 +0300

    edites

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,5 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
@@ -17,14 +7,15 @@ import About from "./pages/About";
 import Admin from "./pages/Admin";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/menu" element={<Menu />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
-  </Router>,
-  document.getElementById("root")
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
